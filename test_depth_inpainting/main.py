266e8757607487ff438cf9fda510094a40ae512e
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-07-10 02:13:12 +0000

    main use relative path

--- test_depth_inpainting/main.py
+++ test_depth_inpainting/main.py
@@ -4,10 +4,12 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEPTH_INPAINTING = "/NYUv2Revisit/depthInpainting/build/depthInpainting"
-DEPTH_IMG_PATH = "/scratchdata/processed/long/depth/10.png"
-MASK_IMG_PATH = "mask.png"
-OUTPUT_IMG_PATH = "out.png"
+DEPTH_IMG_PATH = "/scratchdata/processed/alcove2/depth/10.png"
+MASK_IMG_PATH = os.path.join(FILE_DIR, "mask.png")
+OUTPUT_IMG_PATH = os.path.join(FILE_DIR, "out.png")
 
 # Generate mask
 depth = Image.open(DEPTH_IMG_PATH)
@@ -16,12 +18,12 @@ print(depth.max(), depth.min())
 
 mask = depth != 0
 print(mask.max(), mask.min())
-plt.imsave("mask.png", mask, cmap='gray')
+plt.imsave(MASK_IMG_PATH, mask, cmap='gray')
 
 subprocess.run([DEPTH_INPAINTING, "L", DEPTH_IMG_PATH, MASK_IMG_PATH, OUTPUT_IMG_PATH])
 
 # Color output
 out = Image.open(OUTPUT_IMG_PATH)
 out = np.array(out)
-plt.imsave("out_color.png", out)
+plt.imsave(os.path.join(FILE_DIR,"out_color.png"), out)
 
