0e0e3470fcc95b7839fb187d9e3aa2cb1cb43d3f
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-07-11 05:26:15 +0000

    new iteration exit

--- python/tnnr_apgl.py
+++ python/tnnr_apgl.py
@@ -1,9 +1,18 @@
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-def tnnr_apgl(ori, mask, R, l, eps=0.001):
-    input_norm = np.linalg.norm(ori*mask, 'fro')
+def plot_sort(data, name):
+    print(data[:40])
+    fig, ax = plt.subplots()
+    ax.plot(data, marker='o', linestyle='-', color='blue')
+    ax.set_title('Sorted Singular Values')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Singular Value')
     
+    fig.savefig(name)
+
+def tnnr_apgl(ori, mask, R, l, eps=0.01):
     # SVD decomposition
     
     A, S, Bt = np.linalg.svd(ori, full_matrices=False)
@@ -15,7 +24,10 @@ def tnnr_apgl(ori, mask, R, l, eps=0.001):
     # what matters more is the sequence of t, that is needed to satisfy convergence
     t = 1.0
     X = ori.copy()
-    Y = AB.copy()
+    Y = ori.copy()
+    
+    sigma_r_init = S[R]
+    smallest = 1.0
     
     pbar = tqdm(range(1000), desc="APGL Iterations")
     for i in pbar:
@@ -23,6 +35,11 @@ def tnnr_apgl(ori, mask, R, l, eps=0.001):
         Xlast = X.copy()
         temp = Y + t * (AB - l * (Y - ori) * mask)
         u, sigma, vt = np.linalg.svd(temp, full_matrices=False)
+    
+        if sigma[R]/ sigma_r_init - smallest > eps:
+            break
+        smallest = min(smallest, sigma[R] / sigma_r_init)
+    
         sigma = np.maximum(sigma - t, 0)
         X = u @ np.diag(sigma) @ vt
         
@@ -45,10 +62,6 @@ def tnnr_apgl(ori, mask, R, l, eps=0.001):
         pbar.set_postfix({
             'Frobenius Norm': X_diff,
             'Objective Value': objective_value
-        })    
-        
-        if X_diff / input_norm < eps:
-            print(f"Converged after {i+1} iterations.")
-            break
+        })   
         
     return X
